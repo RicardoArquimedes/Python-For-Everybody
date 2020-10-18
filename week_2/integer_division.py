4ecf178bbe2aaa4fb5be2b367d2a86be4a29dda4
RicardoArquimedes <richardcqt28@gmail.com>
2020-10-18 19:07:09 +0000

    printing integer division

---
+++ week_2/integer_division.py
@@ -0,0 +1,21 @@
+# Integer division produces a floating point result
+
+a = (10 / 2)
+print(a)
+print(type(a)) 
+
+b = (9 / 12)
+print(b)
+print(type(b))
+
+c = (99 / 100)
+print(c)
+print(type(c))
+
+d = (10.0 / 2.0)
+print(d)
+print(type(d)) 
+
+e = (99.0 / 100.0)
+print(e)
+print(type(e)) 
